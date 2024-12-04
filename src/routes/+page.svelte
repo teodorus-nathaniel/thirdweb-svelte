37d86844437760870f0c9ca49b30a152c2d7fa1e
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-12-04 11:55:32 +0700

    feat: init shadcn ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,5 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
+</script>
+
+<Button>asdfasdf</Button>
