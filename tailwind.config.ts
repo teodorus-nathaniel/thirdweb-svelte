091233367e1b56932a54ebb352fbac6d512945dd
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-12-04 14:08:53 +0700

    feat: add twsv- prefix to all css variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,44 +16,44 @@ const config: Config = {
 		},
 		extend: {
 			colors: {
-				border: 'hsl(var(--border) / <alpha-value>)',
-				input: 'hsl(var(--input) / <alpha-value>)',
-				ring: 'hsl(var(--ring) / <alpha-value>)',
-				background: 'hsl(var(--background) / <alpha-value>)',
-				foreground: 'hsl(var(--foreground) / <alpha-value>)',
+				border: 'hsl(var(--twsv-border) / <alpha-value>)',
+				input: 'hsl(var(--twsv-input) / <alpha-value>)',
+				ring: 'hsl(var(--twsv-ring) / <alpha-value>)',
+				background: 'hsl(var(--twsv-background) / <alpha-value>)',
+				foreground: 'hsl(var(--twsv-foreground) / <alpha-value>)',
 				primary: {
-					DEFAULT: 'hsl(var(--primary) / <alpha-value>)',
-					foreground: 'hsl(var(--primary-foreground) / <alpha-value>)'
+					DEFAULT: 'hsl(var(--twsv-primary) / <alpha-value>)',
+					foreground: 'hsl(var(--twsv-primary-foreground) / <alpha-value>)'
 				},
 				secondary: {
-					DEFAULT: 'hsl(var(--secondary) / <alpha-value>)',
-					foreground: 'hsl(var(--secondary-foreground) / <alpha-value>)'
+					DEFAULT: 'hsl(var(--twsv-secondary) / <alpha-value>)',
+					foreground: 'hsl(var(--twsv-secondary-foreground) / <alpha-value>)'
 				},
 				destructive: {
-					DEFAULT: 'hsl(var(--destructive) / <alpha-value>)',
-					foreground: 'hsl(var(--destructive-foreground) / <alpha-value>)'
+					DEFAULT: 'hsl(var(--twsv-destructive) / <alpha-value>)',
+					foreground: 'hsl(var(--twsv-destructive-foreground) / <alpha-value>)'
 				},
 				muted: {
-					DEFAULT: 'hsl(var(--muted) / <alpha-value>)',
-					foreground: 'hsl(var(--muted-foreground) / <alpha-value>)'
+					DEFAULT: 'hsl(var(--twsv-muted) / <alpha-value>)',
+					foreground: 'hsl(var(--twsv-muted-foreground) / <alpha-value>)'
 				},
 				accent: {
-					DEFAULT: 'hsl(var(--accent) / <alpha-value>)',
-					foreground: 'hsl(var(--accent-foreground) / <alpha-value>)'
+					DEFAULT: 'hsl(var(--twsv-accent) / <alpha-value>)',
+					foreground: 'hsl(var(--twsv-accent-foreground) / <alpha-value>)'
 				},
 				popover: {
-					DEFAULT: 'hsl(var(--popover) / <alpha-value>)',
-					foreground: 'hsl(var(--popover-foreground) / <alpha-value>)'
+					DEFAULT: 'hsl(var(--twsv-popover) / <alpha-value>)',
+					foreground: 'hsl(var(--twsv-popover-foreground) / <alpha-value>)'
 				},
 				card: {
-					DEFAULT: 'hsl(var(--card) / <alpha-value>)',
-					foreground: 'hsl(var(--card-foreground) / <alpha-value>)'
+					DEFAULT: 'hsl(var(--twsv-card) / <alpha-value>)',
+					foreground: 'hsl(var(--twsv-card-foreground) / <alpha-value>)'
 				}
 			},
 			borderRadius: {
-				lg: 'var(--radius)',
-				md: 'calc(var(--radius) - 2px)',
-				sm: 'calc(var(--radius) - 4px)'
+				lg: 'var(--twsv-radius)',
+				md: 'calc(var(--twsv-radius) - 2px)',
+				sm: 'calc(var(--twsv-radius) - 4px)'
 			},
 			fontFamily: {
 				sans: [...fontFamily.sans]
