ccec7e06e33a50243fcb6616c7bbc8d47982a71e
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-12-05 13:51:33 +0700

    feat: added skeleton for wallet info

--- src/lib/components/connect-wallet-modal/steps/wallet-selector/wallet-selector-item.svelte
+++ src/lib/components/connect-wallet-modal/steps/wallet-selector/wallet-selector-item.svelte
@@ -4,6 +4,7 @@
 	import type { ConnectWalletModalStep } from '../types.js';
 	import Button from '$/components/ui/button/button.svelte';
 	import { createQuery } from '@tanstack/svelte-query';
+	import Skeleton from '$/components/ui/skeleton/skeleton.svelte';
 
 	export let wallet: Wallet;
 	export let setStep: (step: ConnectWalletModalStep) => void;
@@ -32,9 +33,17 @@
 		class="twsv-w-full twsv-justify-start twsv-gap-3 twsv-p-2 twsv-transition-transform hover:twsv-scale-[1.01]"
 		variant="ghost"
 	>
-		<img class="twsv-h-12 twsv-w-12 twsv-rounded-lg" src={image} alt="" />
+		{#if image}
+			<img class="twsv-h-12 twsv-w-12 twsv-rounded-lg" src={image} alt="" />
+		{:else}
+			<Skeleton class="twsv-h-12 twsv-w-12 twsv-rounded-lg" />
+		{/if}
 		<div class="twsv-flex twsv-flex-col twsv-text-left">
-			<span class="twsv-font-semibold">{walletName}</span>
+			{#if walletName}
+				<span class="twsv-font-semibold">{walletName}</span>
+			{:else}
+				<Skeleton class="twsv-h-4 twsv-w-24" />
+			{/if}
 			{#if installedWalletInfo}
 				<span class="twsv-text-sm twsv-font-normal twsv-text-muted-foreground">Installed</span>
 			{/if}
